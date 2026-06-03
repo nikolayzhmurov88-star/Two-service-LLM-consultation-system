fa09bcb4ecfc7664236301e9965f6845813f6c30
NickZ <nikolayzhmurov88@gmail.com>
2026-06-03 11:57:06 +0300

    Добавлен auth_service/app/core/config.py

---
+++ auth_service/app/core/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings
+from pydantic import ConfigDict
+
+class Settings(BaseSettings):
+    app_name: str = "auth-service"
+    env: str = "local"
+    
+    jwt_secret: str
+    jwt_alg: str = "HS256"
+    access_token_expire_minutes: int = 60
+    
+    sqlite_path: str = "./auth.db"
+    
+    model_config = ConfigDict(env_file=".env", extra="ignore")
+
+settings = Settings()
