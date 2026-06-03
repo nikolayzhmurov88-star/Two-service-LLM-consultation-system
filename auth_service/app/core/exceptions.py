a59e0d3d5e3f0ca4e3ec990a0271a10b79816e34
NickZ <nikolayzhmurov88@gmail.com>
2026-06-03 19:47:32 +0300

    Добавлен auth_service/app/db/base.py

--- auth_service/app/core/exceptions.py
+++ auth_service/app/core/exceptions.py
@@ -1,25 +1,31 @@
 from fastapi import HTTPException, status
 
+# Базовый класс
 class BaseHTTPException(HTTPException):
     def __init__(self, status_code: int, detail: str):
         super().__init__(status_code=status_code, detail=detail)
 
+# Ошибка пользователь уже существует
 class UserAlreadyExistsError(BaseHTTPException):
     def __init__(self):
         super().__init__(status_code=status.HTTP_409_CONFLICT, detail="Пользователь уже существует")
 
+# Ошибка неверная почта или пароль
 class InvalidCredentialsError(BaseHTTPException):
     def __init__(self):
         super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail="Неверная почта или пароль")
 
+# Ошибка неверный токен
 class InvalidTokenError(BaseHTTPException):
     def __init__(self):
         super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail="Неверный токен")
 
+# Ошибка срок действия токена истек
 class TokenExpiredError(BaseHTTPException):
     def __init__(self):
         super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail="Срок действия токена истёк")
 
+# Ошибка пользователь не найден
 class UserNotFoundError(BaseHTTPException):
     def __init__(self):
         super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail="Пользователь не найден")
--- auth_service/app/core/security.py
+++ auth_service/app/core/security.py
@@ -5,18 +5,22 @@ from app.core.config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+# Функция хэширования пароля
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
+# Функция верификации пароля
 def verify_password(plain: str, hashed: str) -> bool:
     return pwd_context.verify(plain, hashed)
 
+# Функция создания токена
 def create_access_token(data: dict) -> str:
     to_encode = data.copy()
     expire = datetime.now(timezone.utc) + timedelta(minutes=settings.access_token_expire_minutes)
     to_encode.update({"exp": expire, "iat": datetime.now(timezone.utc)})
     return jwt.encode(to_encode, settings.jwt_secret, algorithm=settings.jwt_alg)
 
+# Функция проверки токена
 def decode_token(token: str) -> dict:
     try:
         payload = jwt.decode(token, settings.jwt_secret, algorithms=[settings.jwt_alg])
