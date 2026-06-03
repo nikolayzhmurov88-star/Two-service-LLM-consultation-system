90e4fb685a1ac324325ce92d68db05ca0325b880
NickZ <nikolayzhmurov88@gmail.com>
2026-06-03 20:04:45 +0300

    auth_service/app/db/session.py

--- auth_service/app/core/config.py
+++ auth_service/app/core/config.py
@@ -4,14 +4,13 @@ from pydantic import ConfigDict
 # Класс конфигурации приложения
 
 class Settings(BaseSettings):
+    database_url: str = "sqlite+aiosqlite:///./app.db" 
     app_name: str = "auth-service"
     env: str = "local"
     
     jwt_secret: str
     jwt_alg: str = "HS256"
     access_token_expire_minutes: int = 60
-    
-    sqlite_path: str = "./auth.db"
     
     model_config = ConfigDict(env_file=".env", extra="ignore")
 
